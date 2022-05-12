40a527d7519957e37d537ef519422791477100eb
Haltz01 <pedro-x-guerra@hotmail.com>
2022-05-11 22:49:53 -0300

    Adding button (missing interaction)

--- src/commands/GetCardInfo.ts
+++ src/commands/GetCardInfo.ts
@@ -1,8 +1,9 @@
 
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, Interaction, MessageAttachment } from 'discord.js';
-import axios from 'axios';
+import { CommandInteraction, GuildEmoji, Interaction, MessageActionRow, MessageAttachment, MessageButton } from 'discord.js';
+import axios, { AxiosResponse } from 'axios';
 import Canvas from 'canvas';
+import { MessageButtonStyles } from 'discord.js/typings/enums';
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -30,58 +31,67 @@ module.exports = {
 		full_url += `?q=${encodeURI(card_name)}&unique=cards`;
 
 
-		axios.get(full_url).then((res) => {
-			const data = res.data.data[0];
-			// console.log(`[GET_CARD_INFO] getCardInfo: response from database:`);
-			// console.log(data);
-
-			const card_info = {
-				"id": data.id,
-				"mana_cost": data.mana_cost,
-				"type": data.type_line,
-				"name": data.name,
-				"description": data.oracle_text,
-				"set": data.set_name,
-				"rarity": data.rarity,
-				"power": data.power,
-				"thoughness": data.toughness,
-				"legality": data.legalities,
-				"price_usd": data.prices.usd,
-				"card_info_uri": data.uri,
-				"image": data.image_uris.normal
-			};
-
-			console.log(`[GET_CARD_INFO] Card info retrieved from database: ${JSON.stringify(card_info)}`);
-
-			Canvas.loadImage(card_info.image).then((card_img) => {
-				const canvas = Canvas.createCanvas(488, 680);
-				const context = canvas.getContext('2d');
-
-				context.drawImage(card_img, 0, 0, canvas.width, canvas.height);
-
-				context.strokeStyle = "#ffffff";
-
-				const attachment = new MessageAttachment(canvas.toBuffer(), 'card-image.png');
-
-				interaction.followUp({ files: [attachment] });
-			}).catch((err) => {
-				console.log(`[GET_CARD_INFO] Error loading card image: ${err}`);
-			});
-	
-			interaction.followUp(`
-			Card information retrieved from database:
-			====== ${card_info.name} ======
-				Card mana cost: ${card_info.mana_cost}
-				Card type: ${card_info.type}
-				Card description: ${card_info.description}
-				Card set: ${card_info.set}
-				Card rarity: ${card_info.rarity}
-				Card price: US$${card_info.price_usd}
-			=================================================`);
-		}).catch((err) => {
+		let res : AxiosResponse|void = await axios.get(full_url).catch(err => {
 			console.log(`[ERROR] getCardInfo: ${err}`);
 			interaction.followUp(`Error retrieving info for "${card_name}"`);
+			return;
 		});
+
+		if (!res) {
+			return;
+		}
+
+		const data = res.data.data[0];
+
+		// console.log(`[GET_CARD_INFO] getCardInfo: response from database:`);
+		// console.log(data);
 		
+		const card_info = {
+			"id": data.id,
+			"mana_cost": data.mana_cost,
+			"type": data.type_line,
+			"name": data.name,
+			"description": data.oracle_text,
+			"set": data.set_name,
+			"rarity": data.rarity,
+			"power": data.power,
+			"thoughness": data.toughness,
+			"legality": data.legalities,
+			"price_usd": data.prices.usd,
+			"card_info_uri": data.uri,
+			"image": data.image_uris.normal
+		};
+
+		console.log(`[GET_CARD_INFO] Card info retrieved from database: ${JSON.stringify(card_info)}`);
+		
+		let card_img = await Canvas.loadImage(card_info.image).catch((err) => {
+			console.log(`[GET_CARD_INFO] Error loading card image: ${err}`);
+			return null;
+		});
+
+		const row = new MessageActionRow().addComponents(
+			new MessageButton().setCustomId('previousImage').setLabel('Previous Image').setStyle(MessageButtonStyles.PRIMARY),
+			new MessageButton().setCustomId('nextImage').setLabel('Next Image').setStyle(MessageButtonStyles.PRIMARY)
+		)
+
+		const canvas = Canvas.createCanvas(488, 680);
+		const context = canvas.getContext('2d');
+
+		context.drawImage(card_img, 0, 0, canvas.width, canvas.height);
+
+		const attachment = new MessageAttachment(canvas.toBuffer(), 'card-image.png');
+
+		await interaction.followUp({ files: [attachment], components: [row] });
+
+		await interaction.followUp(`
+		Card information retrieved from database:
+		====== ${card_info.name} ======
+			Card mana cost: ${card_info.mana_cost}
+			Card type: ${card_info.type}
+			Card description: ${card_info.description}
+			Card set: ${card_info.set}
+			Card rarity: ${card_info.rarity}
+			Card price: US$${card_info.price_usd}
+		=================================================`);
 	}
 };
